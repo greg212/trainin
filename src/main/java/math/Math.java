9c6caed95c0a653a9f3b6be8965eaeb4a68cd004
Grzegorz Sikora <g.sikora82@gmail.com>
2016-05-24 23:08:45 +0200

    some stuff

---
+++ src/main/java/math/Math.java
@@ -0,0 +1,9 @@
+package math;
+
+/**
+ * Created by gs on 2016-05-24.
+ */
+public class Math {
+
+    // Math.signum
+}
---
+++ src/main/java/strings/FastConcatenation.java
@@ -0,0 +1,14 @@
+package strings;
+
+/**
+ * Created by gs on 2016-05-24.
+ */
+public class FastConcatenation {
+
+   // https://www.quora.com/What-is-the-complexity-of-Java-StringBuffer-append
+
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer();
+        sb.append("ble");
+    }
+}
